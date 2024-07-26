d95769073baf47b476fd3f9eb3ec12b2c3a71018
leehh1997 <leehh1997@naver.com>
2024-07-26 11:47:41 +0900

    web_Controller.java web_Controller.class

--- shopping_mall/src/main/java/com/navershop/www/web_Controller.java
+++ shopping_mall/src/main/java/com/navershop/www/web_Controller.java
@@ -3,11 +3,17 @@ package com.navershop.www;
 import java.io.PrintWriter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class web_Controller {
 	
 	PrintWriter pw = null;
 	
+	@GetMapping("/ajaxok.do")
+	public String ajaxok() {
+		
+		return null;
+	}
 	
 }
